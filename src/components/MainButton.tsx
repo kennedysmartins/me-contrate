34d34f15692f588abe9c2220166fec23bdd8a0fb
KillerDogs <kenybustamante2@hotmail.com>
2023-12-19 18:19:34 -0300

    added confetti and delay

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,6 +1,8 @@
-"use client";
+"use client"
+
 import React, { useState, useEffect, useRef } from "react";
-import Modal from "@/components/Modal"
+import Modal from "@/components/Modal";
+import confetti from "canvas-confetti";
 
 interface ButtonProps {
   move?: boolean;
@@ -13,60 +15,85 @@ const MainButton: React.FC<ButtonProps> = ({ move = false, title }) => {
     x: number;
     y: number;
   } | null>(null);
-  const buttonRef = useRef<HTMLButtonElement | null>(null);
+  const [isMouseOverLocked, setIsMouseOverLocked] = useState(false);
+  const buttonRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const getInitialPosition = () => {
       if (buttonRef.current) {
         const rect = buttonRef.current.getBoundingClientRect();
         setButtonPosition({ x: rect.left, y: rect.top });
+  
+        // Utilize a função de retorno para obter o valor atualizado imediatamente
+
       }
     };
-
+  
     getInitialPosition();
   }, []);
-  const handleButtonClick = () => {
-    if (title === "Sim") {
-      setShowModal(true);
-    } else {
-      const newX = Math.random() * (window.innerWidth - 150);
-      const newY = Math.random() * (window.innerHeight - 150);
-      setButtonPosition({ x: newX, y: newY });
+
+  const handleButtonMouseOver = () => {
+    if (move && buttonPosition && !isMouseOverLocked) {
+      handleButtonMove()
     }
   };
 
-  const handleButtonMouseOver = () => {
-    if (move) {
-      const newX = Math.random() * (window.innerWidth - 150);
-      const newY = Math.random() * (window.innerHeight - 150);
-      setButtonPosition({ x: newX, y: newY });
+  const handleButtonClick = () => {
+    if (title === "Sim") {
+      confetti({
+        particleCount: 100,
+        spread: 360,
+        origin: { y: 0.5 },
+      });
+      setTimeout(() => {
+        setShowModal(true);
+      }, 300);
+    } else {
+      handleButtonMove()
     }
   };
+
+  const handleButtonMove = () => {
+    const newX = Math.random() * (window.innerWidth - 150);
+    const newY = Math.random() * (window.innerHeight - 150);
+    setButtonPosition({ x: newX, y: newY });
+    setIsMouseOverLocked(true);
+    setTimeout(() => {
+      setIsMouseOverLocked(false);
+    }, 200);
+  }
+
+  const handleModalClose = () => {
+    window.location.reload();
+    setShowModal(false);
+  };
+
 
   return (
     <>
-    <div
-    ref={buttonRef as React.RefObject<HTMLDivElement>}
-    onMouseOver={handleButtonMouseOver}
-    className={`rounded text-black px-5 py-2 transition-all duration-500 ease-in-out select-none outline-none ${
-      buttonPosition ? "absolute" : "static"}` }
-    style={{
-      left: buttonPosition ? `${buttonPosition.x}px` : "auto",
-      top: buttonPosition ? `${buttonPosition.y}px` : "auto",
-    }}
-    onClick={handleButtonClick}
-  >
-
-      <a
-        href="#_"
-        className="relative inline-flex items-center justify-center p-4 px-8 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 z-10"
+      
+      <div
+        ref={buttonRef as React.RefObject<HTMLDivElement>}
+        onMouseOver={handleButtonMouseOver}
+        className={`rounded text-black px-5 py-2 transition-all duration-500 ease-in-out select-none outline-none ${
+          buttonPosition ? "absolute" : "static"
+        }`}
+        style={{
+          left: buttonPosition ? `${buttonPosition.x}px` : "auto",
+          top: buttonPosition ? `${buttonPosition.y}px` : "auto",
+        }}
+        onClick={handleButtonClick}
       >
-        <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
-        <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-        <span className="relative text-white">{title}</span>
-      </a>
-    </div>
-    {showModal && <Modal />}
+        <a
+          href="#_"
+          className="relative inline-flex items-center justify-center p-4 px-8 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 z-10"
+        >
+          <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
+          <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
+          <span className="relative text-white">{title}</span>
+        </a>
+      </div>
+      {showModal && <Modal closeModal={handleModalClose} />}
     </>
   );
 };
