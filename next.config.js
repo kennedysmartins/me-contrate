3a050d8a89bb199b096d6fa04fc60bd78ad2538d
KillerDogs <kenybustamante2@hotmail.com>
2023-12-19 18:18:51 -0300

    configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      loader: "default", // Escolha o loader que você deseja usar
+      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**",
+        },
+      ],
+    },
+  }
+  
+  module.exports = nextConfig
+  
