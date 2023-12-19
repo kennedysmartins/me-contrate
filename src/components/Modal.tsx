c415e9f7c28412e7212c6044214bab95663f1f5c
KillerDogs <kenybustamante2@hotmail.com>
2023-12-19 18:17:24 -0300

    modal animations and image

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,22 @@
+import Image from 'next/image';
 import React from 'react';
 
-const Modal = () => {
+const Modal = ({ closeModal }) => {
   return (
-    <div className="fixed inset-0 bg-black text-white flex items-center justify-center z-50">
-      <div className="absolute bg-black text-white flex flex-col items-center p-10">
+    <div className="fixed inset-0 bg-black text-white flex items-center justify-center z-50"
+    style={{ animation: 'slide-up 0.5s ease-out' }}>
+      <div
+        className="absolute bg-black text-white flex flex-col items-center justify-center p-10 animation-delay-2000"
+        style={{ animation: 'slide-up 0.5s ease-out' }}
+      >
         {/* Conteúdo do modal */}
-        <h2 className="text-4xl">Você contratou um funcionário novo, parabéns?</h2>
+        <picture className="rounded-full my-5 overflow-hidden">
+            <Image src={`${process.env.NEXT_PUBLIC_GITHUB_PROFILE}.png`} width='200' height='200' alt=""/>
+        </picture>
+        <h2 className="text-4xl items-center justify-center">Você com certeza fez uma ótima escolha! 🤩</h2>
+        <button onClick={closeModal} className="mt-4 text-gray-700">
+          Voltar
+        </button>
       </div>
     </div>
   );
