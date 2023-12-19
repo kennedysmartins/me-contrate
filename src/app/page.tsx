886f3d61f784b75bd56abfa6759ce1d3a62bbae4
KillerDogs <kenybustamante2@hotmail.com>
2023-12-19 14:21:10 -0300

    fix main page & global style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import MainButton from '@/components/MainButton';
+import React from "react";
+import MainButton from "@/components/MainButton";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24 px-10">
-      <h1 className="flex flex-nowrap">Gostaria de me contratar?</h1>
+    <main
+    className="flex flex-col items-center py-[40dvh] px-10 h-[100dvh]"
+    >
+      <div className="flex items-center justify-center">
+        <h1 className="text-3xl py-5">
+          Gostaria de me contratar?
+        </h1>
+      </div>
       <div className="flex gap-2">
-        <MainButton move={false} title={'Sim'} />
-        <MainButton move={true} title={'Não'} />
+        <MainButton move={false} title={"Sim"} />
+        <MainButton move={true} title={"Não"} />
       </div>
     </main>
   );
