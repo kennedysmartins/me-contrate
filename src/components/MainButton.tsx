43df06c68f1270d26ba8fb03e161f4a5b4828078
KillerDogs <kenybustamante2@hotmail.com>
2024-01-04 16:42:08 -0300

    update component to receive github and linkedin

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -7,9 +7,11 @@ import confetti from "canvas-confetti";
 interface ButtonProps {
   move?: boolean;
   title?: string;
+  github?: string | undefined;
+  linkedin?: string | undefined;
 }
 
-const MainButton: React.FC<ButtonProps> = ({ move = false, title }) => {
+const MainButton: React.FC<ButtonProps> = ({ move = false, title, github, linkedin }) => {
   const [showModal, setShowModal] = useState(false);
   const [buttonPosition, setButtonPosition] = useState<{
     x: number;
@@ -81,16 +83,15 @@ const MainButton: React.FC<ButtonProps> = ({ move = false, title }) => {
         }}
         onClick={handleButtonClick}
       >
-        <a
-          href="#"
+        <div
           className="relative inline-flex items-center justify-center p-4 px-8 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 z-40"
         >
           <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
           <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
           <span className="relative text-white">{title}</span>
-        </a>
+        </div>
       </div>
-      {showModal && <Modal closeModal={handleModalClose} />}
+      {showModal && <Modal  github={github} linkedin={linkedin} closeModal={handleModalClose} />}
     </>
   );
 };
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,39 +1,53 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { MouseEventHandler } from "react";
-import { FaGithub, FaLinkedin  } from "react-icons/fa";
-
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 interface ModalProps {
   closeModal: MouseEventHandler<HTMLButtonElement>;
+  github?: string | undefined;
+  linkedin?: string | undefined;
 }
 
-const Modal: React.FC<ModalProps> = ({ closeModal }) => {
+const Modal: React.FC<ModalProps> = ({ closeModal, github, linkedin }) => {
   return (
     <div className="fixed inset-0 bg-black text-white flex items-center justify-center z-50 animate-slideup">
       <div className="absolute bg-black text-white flex flex-col items-center justify-center p-10">
         {/* Conteúdo do modal */}
         <picture className="rounded-full my-5 overflow-hidden">
-          <Image
-            src={process.env.NEXT_PUBLIC_IMAGE || `${process.env.NEXT_PUBLIC_GITHUB_PROFILE}.png` }
+          {github ? (<Image
+            src={`https://github.com/${github}.png`}
             width="200"
             height="200"
-            alt={`${process.env.NEXT_PUBLIC_FULL_NAME}`}
+            alt={`Imagem de perfil do ${github}`}
             priority
-          />
+          />) :(<Image
+            src={`/choose.png`}
+            width="200"
+            height="200"
+            alt={`Imagem de escolhido`}
+            priority
+          />)}
+  
         </picture>
         <h2 className="md:text-4xl items-center justify-center text-xl">
           Você com certeza fez uma ótima escolha! 🤩
         </h2>
+        <h3 className="md:text-2xl items-center justify-center text-lg mt-2  text-gray-400">
+          Acesse meu perfil abaixo 🔗
+        </h3>
         <div className="flex gap-4 my-4">
-          <Link href={process.env.NEXT_PUBLIC_GITHUB_PROFILE as string} target="_blank">
-            <FaGithub className="text-4xl"/>
-          </Link>
+          {github && (
+            <Link href={`https://github.com/${github}`} target="_blank">
+              <FaGithub className="text-4xl" />
+            </Link>
+          )}
 
-          <Link href={process.env.NEXT_PUBLIC_LINKEDIN_PROFILE as string} target="_blank">
-            <FaLinkedin className="text-4xl"/>
-          </Link>
-
+          {linkedin && (
+            <Link href={`https://linkedin.com/in/${linkedin}`} target="_blank">
+              <FaLinkedin className="text-4xl" />
+            </Link>
+          )}
         </div>
         <button onClick={closeModal} className="mt-4 text-gray-700">
           Voltar
