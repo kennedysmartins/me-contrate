6488fb0de19d98f83798b29777b60c9cd71bfa79
KillerDogs <kenybustamante2@hotmail.com>
2024-02-06 18:10:06 -0300

    fix zindex

--- src/app/git/[username]/page.tsx
+++ src/app/git/[username]/page.tsx
@@ -19,14 +19,14 @@ export async function generateMetadata({ params: { username } }: Params) {
 
 export default function mePage({ params: { username } }: Params) {
   return (
-    <main className="bg-gray-50 flex flex-col items-center py-[40dvh] px-10 h-[100dvh] z-30">
+    <main className="bg-gray-50 flex flex-col items-center py-[40dvh] px-10 h-[100dvh]">
     <div className="relative flex opacity-0 animation-delay-1000 animate-fadeIn">
       <div className="absolute top-0 left-6 lg:w-72 lg:h-72 w-32 h-32 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animation-delay-1000  animate-blob"></div>
       <div className="absolute top-0 -right-4 lg:w-72 lg:h-72 w-32 h-32 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob  animation-delay-3000 "></div>
       <div className="absolute -bottom-20 -left-20 lg:w-72 lg:h-72 w-32 h-32  bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animation-delay-4000  animate-blob"></div>
     </div>
     <div className="flex items-center justify-center">
-      <h1 className="lg:text-3xl text-lg font-bold py-5 z-30 text-gray-800 animate-fadeIn">
+      <h1 className="lg:text-3xl text-lg font-bold py-5 text-gray-800 animate-fadeIn">
         Gostaria de me contratar?
       </h1>
     </div>
