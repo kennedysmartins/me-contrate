b47a438818c6bdb0ba248a95f470cd51c01572be
KillerDogs <kenybustamante2@hotmail.com>
2024-02-06 18:10:00 -0300

    fix zindex

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -86,7 +86,7 @@ const MainButton: React.FC<ButtonProps> = ({ move = false, title, github, linked
         onClick={handleButtonClick}
       >
         <div
-          className="relative inline-flex items-center justify-center p-4 px-8 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500 z-40"
+          className="relative inline-flex items-center justify-center p-4 px-8 py-3 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
         >
           <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
           <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
